66ad98e2167d3bc338f6655c80309e8ae8344d90
Steve Agalloco <steve.agalloco@gmail.com>
2012-08-29 13:35:16 -0400

    configure rspec generators

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ end
 
 module RailsTemplate
   class Application < Rails::Application
+
+    # Rails Generators
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => true,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/support/factories"
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
