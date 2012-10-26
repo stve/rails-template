f1e69ce3d287fb9646ea2ba4dccaaf4dd71c6b06
Steve Agalloco <steve.agalloco@gmail.com>
2012-10-26 11:32:33 -0300

    build should be FactoryGirl.build

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -3,7 +3,7 @@
 FactoryGirl.factories.map(&:name).each do |factory_name|
   describe "The #{factory_name} factory" do
     it 'is valid' do
-      build(factory_name).should be_valid
+      FactoryGirl.build(factory_name).should be_valid
     end
   end
 end
