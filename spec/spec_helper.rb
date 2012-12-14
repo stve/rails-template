7a5a685fd5dfdb06a92c58bee4f2fbd2cc56f986
Steve Agalloco <steve.agalloco@gmail.com>
2012-12-14 12:42:18 -0500

    move capybara-email to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/email/rspec'
 require 'database_cleaner'
 require 'forgery'
 
