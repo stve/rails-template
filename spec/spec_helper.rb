05f805b101cfb7070bb78a508cfb067360d13b01
Steve Agalloco <steve.agalloco@gmail.com>
2013-05-20 23:52:15 -0400

    move simplecov to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
 require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '.bundle'
+end
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] = 'test'
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
