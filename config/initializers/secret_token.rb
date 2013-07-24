9402670e496511f569461bce4dfa5f6eb2dd9a57
Steve Agalloco <steve.agalloco@gmail.com>
2013-07-24 13:17:28 -0400

    this is wreckless

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailsTemplate::Application.config.secret_token = '56d43aac3cf2012f4a8c53a7fb7439aaf6f1b6ab35e29e07ddeb8e0b21b8a27ff5bb21f7db8b22e80f3e234a3a988207d650fece3e9f474c8ae11552e70fbdec'
+RailsTemplate::Application.config.secret_token = ''
